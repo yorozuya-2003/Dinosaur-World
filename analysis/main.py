fd24d8fcfc91cec3b1970ef67dbfac4811a8e239
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-04-28 03:01:47 +0530

    added tabs dash code for analysis, changed directory structure

---
+++ analysis/main.py
@@ -0,0 +1,38 @@
+from tabs.config import (
+    app,
+    html, dcc, Input, Output,
+)
+from tabs.overview_tab import overview_layout
+from tabs.diet_tab import diet_layout
+from tabs.period_tab import period_layout
+from tabs.type_tab import type_layout
+
+app.layout = html.Div([
+    html.H1('Dinosaur Data Analysis'),
+    dcc.Tabs(id='tabs', value='tab-overview', children=[
+        dcc.Tab(label='Overview', value='tab-overview'),
+        dcc.Tab(label='Period Analysis', value='tab-period'),
+        dcc.Tab(label='Type Analysis', value='tab-type'),
+        dcc.Tab(label='Diet Analysis', value='tab-diet'),
+    ]),
+    html.Div(id='tabs-content')
+])
+
+# render tabs
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-overview':
+        return overview_layout
+    elif tab == 'tab-period':
+        return period_layout
+    elif tab == 'tab-type':
+        return type_layout
+    elif tab == 'tab-diet':
+        return diet_layout
+    else:
+        return html.Div('Error: Invalid tab selected')
+
+# main function
+if __name__ == '__main__':
+    app.run_server(debug=True, port=9000)
