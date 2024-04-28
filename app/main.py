9bfcd14d5d7897390011ea1f8931f3371a4f9b82
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-04-28 08:26:26 +0530

    modified analysis, explore and integrated both - modified directory structures - downloaded and grayscaled the images - changed favicon and page title

---
+++ app/main.py
@@ -0,0 +1,41 @@
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+from app import app
+
+import analysis
+import explore
+
+nav = html.Div([
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Analyze", href="/analysis")),
+                dbc.NavItem(dbc.NavLink("Explore", href="/explore")),
+            ] ,
+            brand="Dinosaurs",
+            brand_href="/analysis",
+            color="dark",
+            dark=True,
+            fixed="top",
+        ), 
+    ])
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    nav, 
+    html.Div(id='page-content', children=[]), 
+])
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/analysis':
+        return analysis.layout
+    if pathname == '/explore':
+        return explore.layout
+    else:
+        return analysis.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
